9272a5f871159f0e41e8a00001bb98c3036cab99
ja-thomas <janek.thomas@web.de>
2015-06-25 15:50:20 +0200

    fixed timeseries_models

--- timeseries_models.R
+++ timeseries_models.R
@@ -1,28 +1,29 @@
 library(MoCap)
 library(data.table)
 library(plyr)
-setwd("Uni/Consulting/Code/")
-
-load("../Data/data_processed.RData")
-
-data[, acceleration_diff := c(NA, diff(acceleration))]
-
-
+library(foreach)
+library(doMC)
 library(forecast)
 
-x <- auto.arima(data[.(1, "Arm kreisen", "Proband 4", "007319145247"), position_x])
+setwd("Uni/Consulting/Code/")
 
+load("../Data/data_full.RData")
 
-unique(data$joint_Nr), unique(data$sensorId), unique(data$person), unique(data$course_ID)
-
-
-data <- ddply(data, ~joint_Nr+sensorId+person+course, forecast_positions)
 
 
 
 forecast_positions <- function(data){
-  data$forecast_x <- abs(auto.arima(data$position_x)$residuals)
-  data$forecast_y <- abs(auto.arima(data$position_y)$residuals)
-  data$forecast_z <- abs(auto.arima(data$position_z)$residuals)
+  
+  data$forecast_x <- tryCatch(as.numeric(abs(ets(data$position_x)$residuals)), 
+                              error = function(e) NA)
+  data$forecast_y <- tryCatch(as.numeric(abs(ets(data$position_y)$residuals)), 
+                              error = function(e) NA)
+  data$forecast_z <- tryCatch(as.numeric(abs(ets(data$position_z)$residuals)), 
+                              error = function(e) NA)
   data
 }
+
+registerDoMC(cores = 4)
+data_full <- ddply(data_full, ~joint_Nr + course_Id + person + sensorId, 
+           forecast_positions, .parallel = TRUE)
+
