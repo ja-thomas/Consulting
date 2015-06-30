de82102646e2cd71629415ba9a40790f4897861d
ja-thomas <janek.thomas@web.de>
2015-06-30 15:34:06 +0200

    fixed index in apply

--- gbm_models.R
+++ gbm_models.R
@@ -7,12 +7,12 @@ library(doMC)
 
 load("../Data/data_full_ts.RData")
 
-doMC::registerDoMC(cores = 4)
+doMC::registerDoMC(cores = 5)
 gbm_models <- llply(0:24, function(j) 
   gbm(formula = deviation ~ kinect_error + abs(camera_distance -2.5) +
        position_change + acceleration + c(NA, diff(acceleration)) + 
        shoulder_angle + azimut + elevation + bone_error + forecast_x + 
-       forecast_y + forecast_z, data = data_full[.(0)],
+       forecast_y + forecast_z, data = data_full[.(j)],
      distribution = "gaussian",  n.trees = 5000, interaction.depth = 1), 
   .parallel = TRUE)
 
