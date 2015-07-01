acd5e9708ccaaed227c77b21b106b8076629cf32
ja-thomas <janek.thomas@web.de>
2015-07-01 11:47:52 +0200

    added error handling

--- gbm_models.R
+++ gbm_models.R
@@ -7,15 +7,14 @@ library(doMC)
 
 load("../Data/data_full_ts.RData")
 
-doMC::registerDoMC(cores = 5)
+doMC::registerDoMC(cores = 4)
 gbm_models <- llply(0:24, function(j) 
-  gbm(formula = deviation ~ kinect_error + abs(camera_distance -2.5) +
+  tryCatch(gbm(formula = deviation ~ kinect_error + abs(camera_distance -2.5) +
        position_change + acceleration + c(NA, diff(acceleration)) + 
        shoulder_angle + azimut + elevation + bone_error + forecast_x + 
        forecast_y + forecast_z, data = data_full[.(j)],
      distribution = "gaussian",  n.trees = 10000, interaction.depth = 1,
-     cv.folds = 5), 
-  .parallel = TRUE)
+     cv.folds = 5), error = function(e) NA), .parallel = TRUE)
 
 
 save(gbm_models, file = "../Data/gbm_models_bigger.RData")
