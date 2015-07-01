79f1f7301cf017b2a16a7ffb0ea7035c27558af7
ja-thomas <janek.thomas@web.de>
2015-07-01 18:37:12 +0200

    added gee estimation

---
+++ gee.R
@@ -0,0 +1,44 @@
+library(gee)
+library(data.table)
+library(plyr)
+
+load("../Data/data_full_ts.RData")
+
+data_full[order(joint_Nr)]
+
+
+gee_model <- gee(formula = deviation ~ kinect_error + 
+                   abs(camera_distance -2.5) + position_change + 
+                   acceleration + c(NA, diff(acceleration)) + 
+                   shoulder_angle + azimut + elevation + bone_error + 
+                   forecast_x + forecast_y + forecast_z, 
+                 id = joint_Nr, data = data_full, family = gaussian, 
+                 corstr = "unstructured")
+
+save(gee_model, file = "../Data/gee_model_unstructured.RData")
+
+
+
+
+subs <- data_full[,c("deviation", "joint_Nr"), with = FALSE]
+
+
+x <- lapply(0:24, function(j) subs[.(j)]$deviation)
+
+z <- do.call(cbind, x)
+
+colnames(z) <- paste0("joint_", 0:24)
+correlation <- cor(z)
+save(correlation, file = ("../Data/estimated_correlation.RData"))
+
+gee_model_fixed <- gee(formula = deviation ~ kinect_error + 
+                   abs(camera_distance -2.5) + position_change + 
+                   acceleration + c(NA, diff(acceleration)) + 
+                   shoulder_angle + azimut + elevation + bone_error + 
+                   forecast_x + forecast_y + forecast_z, 
+                 id = joint_Nr, data = data_full, family = gaussian, 
+                 corstr = "fixed", R = correlation)
+
+
+save(gee_model_fixed, file = "../Data/gee_model_fixed.RData") 
+
