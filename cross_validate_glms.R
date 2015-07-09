11c9779df3a197afc586d247f8e382a124c6576a
ja-thomas <janek.thomas@web.de>
2015-07-09 21:46:29 +0200

    fixed wrong brackets

--- cross_validate_glms.R
+++ cross_validate_glms.R
@@ -11,7 +11,7 @@ load("../Data/data_complete.RData")
 
 
 
-cross_validate_join <- function(one_joint_frame, ...){
+cross_validate_join_lm <- function(one_joint_frame, ...){
   
   ddply(one_joint_frame, ~course_Id + person + sensorId, 
         function(test_set){
@@ -37,13 +37,39 @@ cross_validate_join <- function(one_joint_frame, ...){
         })
 }
 
+cross_validate_join_gamma <- function(one_joint_frame, ...){
+  
+  ddply(one_joint_frame, ~course_Id + person + sensorId, 
+        function(test_set){
+          
+          train_index <- setdiff(one_joint_frame$ID, test_set$ID)      
+          
+          train_set <- one_joint_frame[one_joint_frame$ID %in% train_index, ]
+          
+          model <- glm(formula = deviation ~ kinect_error + 
+                         abs(camera_distance -2.5) + 
+                         acceleration + acceleration_diff +
+                         z_fraction + bone_error +
+                         shoulder_angle + azimut + elevation + 
+                         forecast_x + forecast_y + forecast_z, 
+                       data = train_set, ...)
+          
+          test_set$pred_deviation_log_gamma <- predict.glm(model,
+                                                          test_set,
+                                                          type = "response")
+          
+          test_set
+          
+        })
+}
+
 registerDoMC(cores = 4)
-data_full_predicted <- ddply(data_full_predicted, ~joint_Nr, 
-                             cross_validate_join, family = gaussian(link = "log"),
+data_full_predicted <- ddply(data_full, ~joint_Nr, 
+                             cross_validate_join_lm, family = gaussian(link = "log"),
                              .parallel = TRUE)
 
 data_full_predicted <- ddply(data_full_predicted, ~joint_Nr, 
-                             cross_validate_join, family = Gamma(link = "log"),
+                             cross_validate_join_gamma, family = Gamma(link = "log"),
                              .parallel = TRUE)
 
 
--- mboost.R
+++ mboost.R
@@ -14,10 +14,10 @@ mboost_models <- dlply(data_full, ~joint_Nr, function(set){
                shoulder_angle + periphery_x + periphery_y + 
                bone_error + forecast_x + forecast_y + 
                forecast_z + z_fraction, 
-             data = as.data.frame(data_full[.(j)]),
+             data = set,
              family = GammaReg(),
              control = boost_control(mstop = 5000,
-                                     nu = 0.1)
+                                     nu = 0.1))
 }, .parallel = TRUE)
   
   
