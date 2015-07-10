d43cc27e1180ca42f1cbaec4dcf92077f18f5475
ja-thomas <janek.thomas@web.de>
2015-07-10 16:30:30 +0200

    can't save the complete model

--- mboost.R
+++ mboost.R
@@ -7,37 +7,43 @@ library(doMC)
 
 load("../Data/data_complete.RData")
 
-
-ctrl <- ctree_control(maxdepth = 2)
-
-for(j in 0:24){
-  cat(paste("Start mboost model for joint", j, "at:", Sys.time(), "\n"))
+calculate_mboost_model <- function(data_one_joint){
+  
+  ctrl <- ctree_control(maxdepth = 2)
   
   mboost_model <- mboost(deviation ~ 
-           btree(abs_dist, tree_controls = ctrl) + 
-           btree(acceleration, tree_controls = ctrl) + 
-           btree(kinect_error, tree_controls = ctrl) + 
-           btree(acceleration_diff, tree_controls = ctrl) + 
-           btree(shoulder_angle, tree_controls = ctrl) + 
-           btree(periphery_x, tree_controls = ctrl) + 
-           btree(periphery_y, tree_controls = ctrl) + 
-           btree(bone_error, tree_controls = ctrl) + 
-           btree(forecast_x, tree_controls = ctrl) + 
-           btree(forecast_y, tree_controls = ctrl) + 
-           btree(forecast_z, tree_controls = ctrl) +
-           btree(z_fraction, tree_controls = ctrl), 
-         data = as.data.frame(data_full[.(j)]),
-         family = GammaReg(),
-         control = boost_control(mstop = 5000,
-                                 nu = 0.1,
-                                 trace = TRUE))
+                           btree(abs_dist, tree_controls = ctrl) + 
+                           btree(acceleration, tree_controls = ctrl) + 
+                           btree(kinect_error, tree_controls = ctrl) + 
+                           btree(acceleration_diff, tree_controls = ctrl) + 
+                           btree(shoulder_angle, tree_controls = ctrl) + 
+                           btree(periphery_x, tree_controls = ctrl) + 
+                           btree(periphery_y, tree_controls = ctrl) + 
+                           btree(bone_error, tree_controls = ctrl) + 
+                           btree(forecast_x, tree_controls = ctrl) + 
+                           btree(forecast_y, tree_controls = ctrl) + 
+                           btree(forecast_z, tree_controls = ctrl) +
+                           btree(z_fraction, tree_controls = ctrl), 
+                         data = data_one_joint,
+                         family = GammaReg(),
+                         control = boost_control(mstop = 5,
+                                                 nu = 0.1))
   
-  print(mboost_model)
   
-  save(mboost_model, file = paste0("../Data/mboost_big_model_joint_",j,".RData"))
-  cat(paste("finished mboost model for joint", j, "at:", Sys.time(), "\n\n"))
-
+  list(cbind(
+    data_one_joint[,c("timestamp", "sensorId", 
+                             "person", "course_Id", "joint_Nr"),],
+    predicted_mboost = predict(mboost_model, type = "response")),
+  variable_importance = mboost_model$xselect())
 }
+
+
+registerDoMC(cores = 10)
+mboost_models <- dlply(data_full, ~joint_Nr, calculate_mboost_model,
+                  .parallel = TRUE)
+
+save(mboost_models, file = "../Data/mboost_model_summary.RData")
+
 
 
   
